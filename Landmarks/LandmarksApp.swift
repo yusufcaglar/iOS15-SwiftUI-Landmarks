8f4f91e3587e24d14158312037f08a3f68c74057
Yusuf Çağlar <faf7faf7@gmail.com>
2021-09-21 16:33:51 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Yusuf Çağlar on 21.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
